28d13021c6603a2bbb94973106b2e07e4cdaf8c0
jagn <jaegun0103@ajou.ac.kr>
2022-08-03 14:54:42 +0900

    feat: root page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './components/pages/Home';
+import SignUp from './components/pages/SignUp';
+import Header from './components/UI/molescules/Header';
+import HeaderContainer from './components/UI/organisms/HeaderContainer';
+
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+       <HeaderContainer />
+       <Routes>
+        <Route path='/' element={<Home />}></Route>
+       </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
