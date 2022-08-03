3861e77af990666ddc680c2e458d267ae42f8216
jagn <jaegun0103@ajou.ac.kr>
2022-08-03 14:59:46 +0900

    feat: header container 추가

---
+++ src/components/UI/organisms/HeaderContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../molescules/Header";
+import LoginButton from "../molescules/LoginButton";
+
+function HeaderContainer() {
+  return(
+    <Header>
+      <LoginButton />
+    </Header>
+  );
+}
+
+export default HeaderContainer;
